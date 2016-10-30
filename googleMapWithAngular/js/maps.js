92abee5f2350ae3b514f04d3bfb88e6007732743
28kayak <kaya.ota@sjsu.edu>
2016-10-30 16:15:48 -0700

    ok to drop pin

--- googleMapWithAngular/js/maps.js
+++ googleMapWithAngular/js/maps.js
@@ -35,63 +35,87 @@ var cities = [
 //Angular App Module and Controller
 var sampleApp = angular.module('mapsApp', []);
 
-sampleApp.controller('MapCtrl', function ($scope) {
+sampleApp.controller('MapCtrl', ['$scope',function ($scope) {
+
+    $scope.success = function(position){
+        console.log("in success");
+
+        //define google map
+        //$scope.map
+        // $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        // $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        $scope.markers = [];
+        //get current positions
+        console.log(position);
+        $scope.latitued = position.coords.latitude;
+        $scope.longitude = position.coords.longitude;
+        console.log("latitude: "+ $scope.latitued + "longitude: " + $scope.longitude );
+        //define map option using getCurrent position
+        var currentPos =  {lat: $scope.latitued, lng: $scope.longitude };
+        var mapOptions = {
+            zoom: 15,
+            center: currentPos,
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        }
+        //define google map
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        var infoWindow = new google.maps.InfoWindow();
+
+        /*var createMarker = function (info){
+
+            var marker = new google.maps.Marker({
+                map: $scope.map,
+                position: new google.maps.LatLng(info.lat, info.long),
+                title: info.city
+            });
+            marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+
+            google.maps.event.addListener(marker, 'click', function(){
+                infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                infoWindow.open($scope.map, marker);
+            });
+
+            $scope.markers.push(marker);
+
+        }//createMarker
+
+        for (i = 0; i < cities.length; i++){
+            createMarker(cities[i]);
+        }//for
+        */
+        //set initial marker
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: currentPos
+        });
+        $scope.openInfoWindow = function(e, selectedMarker){
+            e.preventDefault();
+            google.maps.event.trigger(selectedMarker, 'click');
+        }//openInfoWindow
+
+
+    }//success
 
     //define mapOption
     //center: current position
     //zoom: 15
+    //$scope.map = ;
+    //$scope.markers;
+
     console.log("in controller");
     if (!navigator.geolocation){
         output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
         return;
-    }
-    console.log("before calling get Current position");
-    navigator.geolocation.getCurrentPosition(success, err);
+    }//if
+    console.log("before calling get Current position")
+
+    navigator.geolocation.getCurrentPosition($scope.success);
 
 
-});//controller
-function success($scope) {
-    var mapOptions = {
-        zoom: 4,
-        center: new google.maps.LatLng(25,80),
-        mapTypeId: google.maps.MapTypeId.TERRAIN
-    }
+}]);//controller
 
-    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    $scope.markers = [];
-
-    var infoWindow = new google.maps.InfoWindow();
-
-    var createMarker = function (info){
-
-        var marker = new google.maps.Marker({
-            map: $scope.map,
-            position: new google.maps.LatLng(info.lat, info.long),
-            title: info.city
-        });
-        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-
-        google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-            infoWindow.open($scope.map, marker);
-        });
-
-        $scope.markers.push(marker);
-
-    }//createMarker
-
-    for (i = 0; i < cities.length; i++){
-        createMarker(cities[i]);
-    }//for
-
-    $scope.openInfoWindow = function(e, selectedMarker){
-        e.preventDefault();
-        google.maps.event.trigger(selectedMarker, 'click');
-    }//openInfoWindow
-
-
-}//success
 function err(){
     console.log("in error function");
     output.innerHTML = "Unable to retrieve your location";
